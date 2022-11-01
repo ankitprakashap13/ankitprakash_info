e4d66762e37b27269d56c65ffc01c7c173ba868e
awol <ankit.ap13@gmail.com>
2022-11-01 19:03:15 +0530

    add post deploy script for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
        "ref"  : "main",
        "repo" : "git@github.com:ankitprakashap13/ankitprakash_info.git",
        "path" : "/var/www/ankitprakash_info",
-       "post-deploy" : "npm install"
+       "post-deploy" : "pm2 startOrRestart ecosystem.config.js --env production"
     }
   }
 }
