7d65506a99c4b5eb00a685626bf494a3623bcaa6
awol <ankit.ap13@gmail.com>
2022-11-01 19:30:30 +0530

    Update script file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "ankitprakash_info",
-    script : "./app.js",
+    script : "./ecosystem.config.js",
     env_production : {
       NODE_ENV : "production",
     }
